14099c21dbdc79459440a4a2ec94be9f942e3a18
Georgi Angelov <gtangelov76@gmail.com>
2026-03-07 20:56:33 +0200

    Update digits_letters_and_other.py

--- Lab/digits_letters_and_other.py
+++ Lab/digits_letters_and_other.py
@@ -4,3 +4,16 @@ digits = ""
 letters = ""
 symbols = ""
 
+for char in string:
+    if char.isdigit():
+        digits += char
+
+    elif char.isalpha():
+        letters += char
+
+    elif not char.isalnum():
+        symbols += char
+
+print(digits)
+print(letters)
+print(symbols)
