215224584591dd83aeaa49d24ec9d4424434d510
Georgi Angelov <gtangelov76@gmail.com>
2026-03-08 20:04:33 +0200

    Create winning_ticket.py

---
+++ Exercise/winning_ticket.py
@@ -0,0 +1,15 @@
+def ticket_is_valid(each_ticket: str) -> bool:
+    if len(ticket) == 20:
+        return True
+    return False
+
+def symbol_finder(left, right):
+    
+    for left, right:
+box_of_tickets = input().split(", ")
+for ticket in box_of_tickets:
+    if ticket_is_valid(ticket):
+        left_side = ticket[:10]
+        right_side = ticket[10:]
+
+        if
