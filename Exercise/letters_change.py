005d86e5bb0177aaf2cd80afb63dba496db0ee48
Georgi Angelov <gtangelov76@gmail.com>
2026-03-09 16:49:55 +0200

    Update letters_change.py

--- Exercise/letters_change.py
+++ Exercise/letters_change.py
@@ -1,30 +1,28 @@
 def upper(current_char:str) -> bool:
-    if current_char.isupper():
-        return True
-    return False
+    return current_char.isupper()
 
 
 def lower(current_char:str) -> bool:
-    if current_char.islower():
-        return True
-    return False
+    return current_char.islower()
 
 
 sequence_of_strings = input().split()
 result = 0
 for string in sequence_of_strings:
     current_number = ""
-    for index in range(len(string)):
-        if string[index].isdigit():
-            current_number += string[index]
+    
+    for char in string:
+        if char.isdigit():
+            current_number += char
+
     first_letter = string[0]
     last_letter = string[-1]
 
     if upper(first_letter):
-        result += int(current_number) / ((ord(string[0])) - 64)
+        result += int(current_number) / ((ord(first_letter)) - 64)
 
     elif lower(first_letter):
-        result += int(current_number) * ((ord(string[0])) - 96)
+        result += int(current_number) * ((ord(first_letter)) - 96)
 
     if upper(last_letter):
         result -= ord(last_letter) - 64
